0e376228fd3ebfcbaacefb0f05cc7f95d80b84c2
EDSON_MGP <leoedison.1996@gmail.com>
2020-02-13 21:37:49 -0500

    Updates

---
+++ precache-manifest.a9d500341576c86eab07cf7c44dc25f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0bb022cc071c27ed8b3508addf5a21a",
+    "url": "/cibertec-react/index.html"
+  },
+  {
+    "revision": "5ce45be7be4564feca5e",
+    "url": "/cibertec-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "50f52586bb7d736abaa5",
+    "url": "/cibertec-react/static/css/main.12e04618.chunk.css"
+  },
+  {
+    "revision": "5ce45be7be4564feca5e",
+    "url": "/cibertec-react/static/js/2.074ebaf5.chunk.js"
+  },
+  {
+    "revision": "dd897ee3cfa67e5c2ebba71b5eb1cda6",
+    "url": "/cibertec-react/static/js/2.074ebaf5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "50f52586bb7d736abaa5",
+    "url": "/cibertec-react/static/js/main.27b1b477.chunk.js"
+  },
+  {
+    "revision": "73981c138432b7dedd22",
+    "url": "/cibertec-react/static/js/runtime-main.6981c0b3.js"
+  },
+  {
+    "revision": "a1e184b8b64d0a5850f0b9a8dad670f8",
+    "url": "/cibertec-react/static/media/loading.a1e184b8.svg"
+  }
+]);
